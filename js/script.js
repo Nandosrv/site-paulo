90a0825e5bbe7105a78b48c7059ee9897c8d20dd
Nandosrv <nandooliveirasrv8@gmail.com>
2024-09-22 22:23:42 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+    $('#mobile_btn').on('click', function (){
+         $('#links').toggleClass('active')
+        $('#mobile_btn').find('i').toggleClass('fa-x')
+        })
+})
